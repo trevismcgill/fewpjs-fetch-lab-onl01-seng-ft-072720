582eee086e1d3dfb15e5d3c7492462184472d7a9
trevismcgill <trevismcgill25@gmail.com>
2020-11-03 11:16:44 -0600

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
-
-}
+    fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+    return fetch("https://anapioficeandfire.com/api/books")
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main')
@@ -8,9 +11,9 @@ function renderBooks(books) {
     const h2 = document.createElement('h2')
     h2.innerHTML = book.name
     main.appendChild(h2)
-  })
-}
+  });
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
-})
+});
